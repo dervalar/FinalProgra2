3c1447b0ce4cf774ccaa074854867d38a8d64593
Dervalar <lisandro.mf@hotmail.com>
2026-07-01 19:57:00 -0300

    Proyecto base del final

---
+++ Model/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Model
+{
+    internal class Cliente
+    {
+        public string nombreCompleto {  get; set; }
+        public string DNI {  get; set; }
+        public string email { get; set; }   
+
+    }
+}
---
+++ Model/Instrumento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Model
+{
+    internal class Instrumento
+    {
+        public string nombre { get; set; }
+        public string categoria { get; set; } // (Cuerda / Viento / Percusión / Teclado)
+        public string marca { get; set; }
+        public int codigo { get; set; }
+        public bool disponible { get; set; }
+
+
+    }
+}
